be7e99cde06ac042fdb0c2cb2a18ca257126749d
10ErickHerrera <10erickherrera@gmail.com>
2024-11-18 17:26:03 +0000

    avance

---
+++ src/js/component/personajes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+//create your first component
+const Personajes = () => {
+
+function obtenerPersonajes(){
+    
+}
+
+	return (
+		<div className="text-center">
+			<h1>Personajes:</h1>
+		</div>
+	);
+};
+
+export default Personajes;
